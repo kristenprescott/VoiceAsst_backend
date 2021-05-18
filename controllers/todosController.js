3b07fffeb29ecd813559c631b25b220081f306be
Kristen Prescott <prescottdarling@gmail.com>
2021-05-18 14:58:25 -0400

    Made new todosController.js with more specific routes

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -2,21 +2,31 @@
 const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
+/*
+|----------------------------------------------------------
+| Todos Route
+|----------------------------------------------------------
+| [Method]  [Route]
+| GET       /todos          
+| POST      /todos/new
+| GET       /todos/:id
+| POST      /todos:id   
+| PATCH     /todos:id                           
+| PUT       /todos:id          
+| DELETE    /todos:id          
+| 
+*/
 
+// METHOD  : GET
+// ROUTE   : /todos
+// FUNCTION: Get all todos
 // INDEX
 router.get("/", async (req, res) => {
-  let filters;
-  if (Object.keys(req.query).length > 0) {
-    filters = { ...req.query };
-  }
   try {
-    if (!filters) {
-      const foundTodos = await Todo.find({});
-      res.status(200).json(foundTodo);
-    } else {
-      const foundTodos = await Todo.find({ ...filters });
-      res.status(200).json(foundTodos);
-    }
+    //Find and sort todos by creation date
+    const todos = await Todo.find().sort([["createdAt", -1]]);
+
+    res.send(todos);
   } catch (error) {
     res.status(400).json({
       msg: error.message,
@@ -24,11 +34,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+// METHOD  : POST
+// ROUTE   : /new
+// FUNCTION: Add a new todo
 // CREATE
 router.post("/new", async (req, res) => {
   try {
-    const createdTodo = await Todo.create(req.body);
-    res.status(200).json(createdTodo);
+    const todo = await Todo.create({
+      task: req.body.task,
+      createdAt: Date.now(),
+    });
+
+    await todo.save();
+    res.send(todo);
   } catch (error) {
     res.status(400).json({
       msg: error.message,
@@ -36,13 +54,75 @@ router.post("/new", async (req, res) => {
   }
 });
 
-// UPDATE
+// METHOD  : GET
+// ROUTE   : /:id
+// FUNCTION: Fetch a todo
+// INDEX Specific todo
+router.get("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+
+    res.send(todo);
+  } catch (error) {
+    res.status(400).json({
+      msg: error.message,
+    });
+  }
+});
+
+// METHOD  : POST
+// ROUTE   : /todos:id
+// FUNCTION: Toggle todo to be done or not
+//
+router.post("/:id", async (req, res) => {
+  try {
+    const todoRef = await Todo.findById(req.params.id);
+    const todo = await Todo.findOneAndUpdate(
+      { _id: req.params.id },
+      { done: !todoRef.done }
+    );
+
+    await todo.save();
+    res.send(todo);
+  } catch (error) {
+    res.status(400).json({
+      msg: error.message,
+    });
+  }
+});
+
+// METHOD  : PATCH
+// ROUTE   : /todos:id
+// FUNCTION: Modify the todo
+router.patch("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findOneAndUpdate(
+      { _id: req.params.id },
+      { task: req.body.task }
+    );
+    await todo.save();
+
+    res.send(todo);
+  } catch (error) {
+    res.status(400).json({
+      msg: error.message,
+    });
+  }
+});
+
+// METHOD  : PUT
+// ROUTE   : /todos:id
+// FUNCTION: Update the todo
 router.put("/:id", async (req, res) => {
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.status(200).json({ updatedTodo });
+    const todo = await Todo.findByIdAndUpdate(
+      { _id: req.params.id },
+      { task: req.body.task },
+      { new: true }
+    );
+    await todo.save();
+
+    res.send(todo);
   } catch (error) {
     res.status(400).json({
       msg: error.message,
@@ -50,11 +130,14 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-// DELETE
+// METHOD  : DELETE
+// ROUTE   : /todos:id
+// FUNCTION: Delete the todo
 router.delete("/:id", async (req, res) => {
   try {
-    const deletedTodo = await Todo.findByOneAndDelete(req.params.id);
-    res.status(200).json(deletedTodo);
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+
+    res.send(todo);
   } catch (error) {
     res.status(400).json({
       msg: error.message,
