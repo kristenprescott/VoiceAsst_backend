8a6d3fb6bd244862ecaca98674bef48a037f5c92
Kristen Prescott <prescottdarling@gmail.com>
2021-05-20 21:40:11 -0400

    ugh

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  // useFindAndModify: false,
 });
 mongoose.connection.once("connected", () =>
   console.log("Connected to Mongo - life is good B^)")
