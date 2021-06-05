dcf7b85a1e5de302afbab271e69bd75815949efa
Kristen Prescott <prescottdarling@gmail.com>
2021-06-05 17:16:06 -0400

    nothing major

--- server.js
+++ server.js
@@ -119,16 +119,76 @@ app.post("/login", (req, res) => {
   });
 });
 ////////////////////////////////////////////////////////////
+// SPOTIFY
+/**
+ * This is an example of a basic node.js script that performs
+ * the Client Credentials oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
+
+var request = require("request"); // "Request" library
+
+var client_id = "CLIENT_ID"; // Your client id
+var client_secret = "CLIENT_SECRET"; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: "https://accounts.spotify.com/api/token",
+  headers: {
+    Authorization:
+      "Basic " + new Buffer(client_id + ":" + client_secret).toString("base64"),
+  },
+  form: {
+    grant_type: "client_credentials",
+  },
+  json: true,
+};
+
+request.post(authOptions, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: "https://api.spotify.com/v1/users/jmperezperez",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      json: true,
+    };
+    request.get(options, function (error, response, body) {
+      console.log(body);
+    });
+  }
+});
+app.get("/login", function (req, res) {
+  var scopes = "user-read-private user-read-email";
+  res.redirect(
+    "https://accounts.spotify.com/authorize" +
+      "?response_type=code" +
+      "&client_id=" +
+      my_client_id +
+      (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+      "&redirect_uri=" +
+      encodeURIComponent(redirect_uri)
+  );
+});
+
+////////////////////////////////////////////////////////////
 // *~*~*~ Prettier Error Handling For Server Crashes *~*~*~
 ////////////////////////////////////////////////////////////
 const server = app.listen(PORT, () => {
-  console.log("╭──────────────────────────────────╮");
-  console.log("│꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷│");
-  console.log(`│꒦꒷꒦꒷꒦꒷꒦ PORT ꒦꒷꒦꒷꒦꒷꒦│`);
-  console.log(`│꒷꒦꒷꒦꒷꒦꒷꒦${PORT}꒦꒷꒦꒷꒦꒷꒦│`);
-  console.log("│꒷꒦꒷꒦꒷꒦꒷꒦꒷ʕ•ᴥ•ʔ.꒦꒷꒦꒷꒦│");
-  console.log("│꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷│");
-  console.log("╰──────────────────────────────────╯");
+  console.log("╭────────────────────────────────────╮");
+  console.log("│               *ʕ•ᴥ•ʔ*              │");
+  console.log("│------------------------------------│");
+  console.log("│           ╭------------╮           │");
+  console.log("│           |    PORT    |           │");
+  console.log("│           |------------|           │");
+  console.log(`│           |    ${PORT}    |           │`);
+  console.log("│           ╰------------╯           │");
+  console.log("╰────────────────────────────────────╯");
 });
 
 // process.on("unhandledRejection", (err, promise) => {
